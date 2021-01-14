ed8dc187251faa451c68679aba13fc8ae05c0baa
Luka <Luka Matuzic>
2021-01-14 15:00:32 +0100

    installed react router, wrapped app with it, created empty pages, created navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Projects</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
