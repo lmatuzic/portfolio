37a5a86b93bda5a2c4db02875878e500dd3fa952
Luka <luka@pop-os.localdomain>
2021-01-20 23:00:42 +0100

    loop through json file to show project data

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,49 +1,20 @@
 import React from 'react'
+import projects from '../projectData.json'
 
 function Project() {
-  const projects = [
-    {
-      name: "Asset Manager",
-      description: "Internal web application for managing company assets ",
-      role: "Frontend web development",
-      technologies: "HTML, CSS, SCSS, Javascript, Ruby on Rails",
-      id: 1,
-    },
-    {
-      name: "Komyoon",
-      description: "Web application for storing, sharing and sorting art",
-      role: "Frontend web development",
-      technologies: "HTML, CSS, SCSS, Javascript, Ruby on Rails",
-      id: 2,
-    },
-    {
-      name: "Archibrix",
-      description: "Web application for selling BIM blueprints",
-      role: "Frontend web development",
-      technologies: "HTML, CSS, SCSS, Javascript, Ruby on Rails",
-      id: 3,
-    },
-    {
-      name: "Pink Bibs",
-      description: "Web application for valet parking and keeping your car secure in South Africa",
-      role: "Full stack web development",
-      technologies: "HTML, CSS, SCSS, Javascript, Ruby on Rails",
-      id: 4,
-    },
-  ];
-  
   return (
-    <div className="project">
-      <h4 className="project__name">
-        {
-          projects.map((project) => { 
-            return (
-              project.name
-            ) 
-          })
-        } 
-      </h4>
-    </div>
+    <>
+      {
+        projects.map((project) => { 
+          return (
+            <div key={project.id} className="project" id={project.id}>
+              <h4 className="project__name">{project.name}</h4>
+              <div className="project__description">{project.description}</div>
+            </div>
+          ) 
+        })
+      } 
+    </>
   )
 }
 
