ad343f1dfa27deefef00e67c4fc5d7b2601e5922
Luka <luka@pop-os.localdomain>
2021-01-21 21:05:12 +0100

    added homepage, styled it

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="homepage">
+      <div className="profile__img-wrapper"></div>
+      <div className="profile__info">
+        <h1>Luka Matuzić</h1>
+        <h2>Web developer</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Home
