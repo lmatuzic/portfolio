69237a31947f4966e03ca82ba7443606705c1260
Luka <luka@pop-os.localdomain>
2021-01-22 23:23:23 +0100

    added toggle navigation transition

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import avatar from '../images/me.png'
+//import avatar from '../images/me.png'
 
 function Home() {
   return (
     <div className="homepage">
       <div className="container">
-        <img src={avatar} alt="avatar"/>
-          <div className="profile__info">
+        {/* <img src={avatar} alt="avatar"/> */}
+        <div className="profile__info">
           <h1>Luka Matuzić</h1>
           <h2>Web developer</h2>
         </div>
